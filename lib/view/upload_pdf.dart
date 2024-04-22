db058fdd3a60d3d0c105e3cab7328c65d3de0b3d
Mahek <mahek.karmaleen@gmail.com>
2024-04-22 12:04:45 +0530

    upload pdf imports

--- lib/view/upload_pdf.dart
+++ lib/view/upload_pdf.dart
@@ -1,3 +1,5 @@
+import 'package:dotted_border/dotted_border.dart';
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:path/path.dart' as path;
